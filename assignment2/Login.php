b3a1e73536cbb96d076a0d5a6069523d6d999e82
Orgwan <0r6wan@gmail.com>
2023-11-14 16:32:44 +0300

    Write login file

--- assignment2/Login.php
+++ assignment2/Login.php
@@ -1,27 +1,15 @@
-<!--
--[printf] is a powerful function that controls the format of the output
-by letting you put special formatting characters in a string
--[timestamps] number of seconds since the start of January 1, 1970
--date($format, $timestamp);
+<?php
 
--->
+   $host = 'localhost';
+   $data = 'publications';
+   $user = 'root';
+   $pass = 'assignment2';
+   $chrs = 'utf8mb4';
+   $attr = "mysql:host=$host;dbname=$data;charset=$chrs";
+   $opts =
+   [
+   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+   PDO::ATTR_EMULATE_PREPARES => PDO::false,
 
-<html>
-
-<body>
-
-  <?php
-  
-    printf("There are %d items in your basket", 3); echo "<br> ";
-	
-	printf("The result is: $%.2f", 123.42/12); echo "<br> ";
-  
-  
-  
-  
-    
-  ?> 
-  
-</body>
-
-</html>
+?>
--- assignment2/registration.php
+++ assignment2/registration.php
@@ -1,16 +1,4 @@
-<!--
--[printf] is a powerful function that controls the format of the output
-by letting you put special formatting characters in a string
--[timestamps] number of seconds since the start of January 1, 1970
--date($format, $timestamp);
-
--->
-
-<html>
-
-<body>
-
-  <?php
+<?php
   
     printf("There are %d items in your basket", 3); echo "<br> ";
 	
@@ -20,8 +8,4 @@ by letting you put special formatting characters in a string
   
   
     
-  ?> 
-  
-</body>
-
-</html>
+?>
