2a9c66fbd30d54a3f8b7b2a14e14b4baf1002cc6
Orgwan <0r6wan@gmail.com>
2023-11-14 14:55:01 +0300

    Just Make direct and files

---
+++ assignment2/submit.php
@@ -0,0 +1,27 @@
+<!--
+-[printf] is a powerful function that controls the format of the output
+by letting you put special formatting characters in a string
+-[timestamps] number of seconds since the start of January 1, 1970
+-date($format, $timestamp);
+
+-->
+
+<html>
+
+<body>
+
+  <?php
+  
+    printf("There are %d items in your basket", 3); echo "<br> ";
+	
+	printf("The result is: $%.2f", 123.42/12); echo "<br> ";
+  
+  
+  
+  
+    
+  ?> 
+  
+</body>
+
+</html>
